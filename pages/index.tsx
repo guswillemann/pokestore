313f5128d14679f7b1f366bfe4413e49f5ec900a
Gustavo Willemann <guswillemann@gmail.com>
2021-07-20 01:27:21 -0300

    fix: home title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import styled from 'styled-components';
 
 const HomeWrapper = styled.main`
@@ -69,34 +70,40 @@ const HomeWrapper = styled.main`
 
 export default function Home() {
   return (
-    <HomeWrapper>
-      <h1>Acesse uma PokeStore:</h1>
-      <ul>
-        <a href="/fire">
-          <li className="fire-store">
-            <img src="/images/fire.svg" alt="fire Store" />
-            <p>fire</p>
-          </li>
-        </a>
-        <a href="/grass">
-          <li className="grass-store">
-            <img src="/images/grass.svg" alt="Grass Store" />
-            <p>grass</p>
-          </li>
-        </a>
-        <a href="/rock">
-          <li className="rock-store">
-            <img src="/images/rock.svg" alt="Rock Store" />
-            <p>rock</p>
-          </li>
-        </a>
-        <a href="/water">
-          <li className="water-store">
-            <img src="/images/water.svg" alt="Water Store" />
-            <p>water</p>
-          </li>
-        </a>
-      </ul>
-    </HomeWrapper>
+    <>
+      <Head>
+        <link rel="shortcut icon" href={`/images/pokeball.png`} type="imagem/svg+xml" />
+        <title>PokeStore</title>
+      </Head>
+      <HomeWrapper>
+        <h1>Acesse uma PokeStore:</h1>
+        <ul>
+          <a href="/fire">
+            <li className="fire-store">
+              <img src="/images/fire.svg" alt="fire Store" />
+              <p>fire</p>
+            </li>
+          </a>
+          <a href="/grass">
+            <li className="grass-store">
+              <img src="/images/grass.svg" alt="Grass Store" />
+              <p>grass</p>
+            </li>
+          </a>
+          <a href="/rock">
+            <li className="rock-store">
+              <img src="/images/rock.svg" alt="Rock Store" />
+              <p>rock</p>
+            </li>
+          </a>
+          <a href="/water">
+            <li className="water-store">
+              <img src="/images/water.svg" alt="Water Store" />
+              <p>water</p>
+            </li>
+          </a>
+        </ul>
+      </HomeWrapper>
+    </>
   );
 }
